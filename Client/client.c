1d0b7e05431e4effc13d44a95ed49cbd8431b7e9
NathBel <bellus.nathan@gmail.com>
2023-04-19 14:38:49 +0200

    :white_check_mark: Sprint 1 terminé : ajout de la gestion des erreurs

--- Client/client.c
+++ Client/client.c
@@ -6,7 +6,7 @@
 
 int main(int argc, char *argv[])
 {
-    int id_client = atoi(argv[3]);
+  int id_client = atoi(argv[3]);
   printf("Début programme\n");
   int dS = socket(PF_INET, SOCK_STREAM, 0);
   printf("Socket Créé\n");
@@ -16,54 +16,136 @@ int main(int argc, char *argv[])
   inet_pton(AF_INET, argv[1], &(aS.sin_addr));
   aS.sin_port = htons(atoi(argv[2]));
   socklen_t lgA = sizeof(struct sockaddr_in);
-  connect(dS, (struct sockaddr *)&aS, lgA);
-  printf("Socket Connecté\n");
 
+  if (connect(dS, (struct sockaddr *)&aS, lgA) == -1)
+  {
+    printf("Erreur de connexion\n");
+    exit(1);
+  }
+  else
+  {
+    printf("Socket Connecté\n");
+  }
   int n = 200;
 
   char *msg = malloc(n * sizeof(char));
   char *msg2 = malloc(n * sizeof(char));
   while (1)
   {
-    if ( id_client == 1){
+    if (id_client == 1)
+    {
       printf("Entrez un message ('fin' pour quitter) : ");
       fgets(msg, n, stdin);
       if (msg[strlen(msg) - 1] == '\n')
         msg[strlen(msg) - 1] = '\0';
-      //Envoi de la taille du mess
-      int sizeMess = strlen(msg)+1;
-      
-      send(dS, &sizeMess, 4, 0);
-      //Envoi du mess
-      send(dS, msg,sizeMess, 0);
-      printf("Message Envoyé \n");
-      if(strcmp(msg, "fin") == 0){
+      // Envoi de la taille du mess
+      int sizeMess = strlen(msg) + 1;
+
+      if (send(dS, &sizeMess, 4, 0) == -1)
+      {
+        printf("Erreur d'envoi\n");
+        break;
+      }
+      else
+      {
+        printf("Taille du message : %d\n", sizeMess);
+      }
+      // Envoi du mess
+      if (send(dS, msg, sizeMess, 0) == -1)
+      {
+        printf("Erreur d'envoi\n");
+        break;
+      }
+      else
+      {
+        printf("Message Envoyé \n");
+      }
+
+      if (strcmp(msg, "fin") == 0)
+      {
         shutdown(dS, 2);
         printf("Fin du programme");
       }
-      //Reception taille du mess
-      recv(dS, &sizeMess, sizeof(sizeMess), 0);
-      printf("Message 2 recu de taille : %d\n", sizeMess);
-      //Reception du mess 
-      recv(dS, msg2, sizeMess, 0);
-      printf("Message reçu : %s\n", msg2);
-    } 
-    else if (id_client == 2){
+      // Reception taille du mess
+      if (recv(dS, &sizeMess, sizeof(sizeMess), 0) == -1)
+      {
+        printf("Erreur de reception\n");
+        break;
+      }
+      else
+      {
+        printf("Message 2 recu de taille : %d\n", sizeMess);
+      }
 
-      recv(dS, msg, 20, 0);
-      printf("Message reçu : %s\n", msg);
+      // Reception du mess
+      if (recv(dS, msg2, sizeMess, 0) == -1)
+      {
+        printf("Erreur de reception\n");
+        break;
+      }
+      else
+      {
+        printf("Message reçu : %s\n", msg2);
+      }
+    }
+
+
+    else if (id_client == 2)
+    {
+      int sizeMess;
+      // Reception taille du mess
+      if (recv(dS, &sizeMess, 4, 0) == -1)
+      {
+        printf("Erreur de reception\n");
+        break;
+      }
+      else
+      {
+        printf("Message 2 recu de taille : %d\n", sizeMess);
+      }
+
+      if (recv(dS, msg, 20, 0) == -1)
+      {
+        printf("Erreur de reception\n");
+        break;
+      }
+      else
+      {
+        printf("Message reçu : %s\n", msg);
+      }
 
       printf("Entrez un message ('fin' pour quitter) : ");
       fgets(msg2, n, stdin);
+
       if (msg2[strlen(msg2) - 1] == '\n')
         msg2[strlen(msg2) - 1] = '\0';
-      //Envoi de la taille du mess
-      int sizeMess2 = strlen(msg2)+1;
-      send(dS, &sizeMess2, 4, 0);
-      //Envoi du mess
-      send(dS, msg2,sizeMess2, 0);
+
+      // Envoi de la taille du mess
+      int sizeMess2 = strlen(msg2) + 1;
+      if (send(dS, &sizeMess2, 4, 0) == -1)
+      {
+        printf("Erreur d'envoi\n");
+        break;
+      }
+      else
+      {
+        printf("Taille du message : %d\n", sizeMess2);
+      }
+
+      // Envoi du mess
+      if (send(dS, msg2, sizeMess2, 0) == -1)
+      {
+        printf("Erreur d'envoi\n");
+        break;
+      }
+      else
+      {
+        printf("Message Envoyé \n");
+      }
+
       printf("Message Envoyé \n");
-      if(strcmp(msg2, "fin") == 0){
+      if (strcmp(msg2, "fin") == 0)
+      {
         shutdown(dS, 2);
         printf("Fin du programme");
       }
--- Server/server.c
+++ Server/server.c
@@ -6,26 +6,32 @@
 
 int main(int argc, char *argv[])
 {
-   
-printf("Début programme\n");
 
-        int dS = socket(PF_INET, SOCK_STREAM, 0);
-        printf("Socket Créé\n");
+    printf("Début programme\n");
 
-        struct sockaddr_in ad;
-        ad.sin_family = AF_INET;
-        ad.sin_addr.s_addr = INADDR_ANY;
-        ad.sin_port = htons(atoi(argv[1]));
-        bind(dS, (struct sockaddr *)&ad, sizeof(ad));
+    int dS = socket(PF_INET, SOCK_STREAM, 0);
+    printf("Socket Créé\n");
+
+    struct sockaddr_in ad;
+    ad.sin_family = AF_INET;
+    ad.sin_addr.s_addr = INADDR_ANY;
+    ad.sin_port = htons(atoi(argv[1]));
+    if (bind(dS, (struct sockaddr *)&ad, sizeof(ad)) == -1)
+    {
+        printf("Erreur de bind\n");
+        exit(1);
+    }
+    else
+    {
         printf("Socket Nommé\n");
-
 
         listen(dS, 7);
         printf("Mode écoute\n");
+    }
 
-
-    while(1){
-
+    while (1)
+    {
+        printf("Début de la messagerie\n");
         struct sockaddr_in aC;
         struct sockaddr_in aC2;
         socklen_t lg = sizeof(struct sockaddr_in);
@@ -41,47 +47,107 @@ printf("Début programme\n");
 
         while (!stop)
         {
-            //Reception taille du mess
-            recv(dSC, &sizeMess, sizeof(sizeMess), 0);
-            printf("Message 1 recu de taille : %d\n", sizeMess);
-            //Reception du mess
-            recv(dSC, msg, sizeMess, 0);
-
-            if (strcmp(msg, "fin") != 0)
+            // Reception taille du mess
+            if (recv(dSC, &sizeMess, sizeof(sizeMess), 0) == -1)
             {
-                printf("Message reçu : %s\n", msg);
-                
-                send(dSC2 , &msg, sizeMess, 0);
-                printf("Message Envoyé\n");
+                printf("Erreur de reception\n");
+                break;
             }
             else
             {
-                printf("Fin de la connexion\n");
-                stop = 1;
+                printf("Taille du message à recevoir: %d\n", sizeMess);
             }
 
-            if(stop != 1){
-                
-                //Reception taille du mess
-                recv(dSC2, &sizeMess, sizeof(sizeMess), 0);
-                printf("Message 2 recu de taille : %d\n", sizeMess);
-                //Reception du mess 
-                recv(dSC2, msg, sizeMess, 0);
-
+            // Reception du mess
+            if (recv(dSC, msg, sizeMess, 0) == -1)
+            {
+                printf("Erreur de reception\n");
+                break;
+            }
+            else
+            {
                 if (strcmp(msg, "fin") != 0)
                 {
                     printf("Message reçu : %s\n", msg);
-                    //Envoi de la taille du mess
+                    
+                    // Envoi de la taille du mess
+                    if(send(dSC2, &sizeMess, 4, 0) == -1){
+                        printf("Erreur d'envoi\n");
+                        break;
+                    }
+                    else{
+                        printf("Taille du message à envoyer : %d\n", sizeMess);
+                    }
 
-                    send(dSC, &sizeMess, 4, 0);
-                    //Envoie message
-                    send(dSC, msg, sizeMess, 0);
-                    printf("Message Envoyé\n");
+                    // Envoi de la taille du mess
+                    if(send(dSC2, &msg, sizeMess, 0) == -1){
+                        printf("Erreur d'envoi\n");
+                        break;
+                    }
+                    else{
+                        printf("Message envoyé\n");
+                    }
                 }
                 else
                 {
                     printf("Fin de la connexion\n");
                     stop = 1;
+                }
+            }
+
+            if (stop != 1)
+            {
+
+                // Reception taille du mess
+                if (recv(dSC2, &sizeMess, sizeof(sizeMess), 0) == -1)
+                {
+                    printf("Erreur de reception\n");
+                    break;
+                }
+                else
+                {
+                    printf("Message 2 recu de taille : %d\n", sizeMess);
+                }
+
+                // Reception du mess
+                if (recv(dSC2, msg, sizeMess, 0) == -1)
+                {
+                    printf("Erreur de reception\n");
+                    break;
+                }
+                else
+                {
+                    if (strcmp(msg, "fin") != 0)
+                    {
+                        printf("Message reçu : %s\n", msg);
+                        // Envoi de la taille du mess
+
+                        if (send(dSC, &sizeMess, 4, 0) == -1)
+                        {
+                            printf("Erreur d'envoi\n");
+                            break;
+                        }
+                        else
+                        {
+                            printf("Taille du message envoyée\n");
+                        }
+
+                        // Envoie message
+                        if (send(dSC, msg, sizeMess, 0) == -1)
+                        {
+                            printf("Erreur d'envoi\n");
+                            break;
+                        }
+                        else
+                        {
+                            printf("Message Envoyé\n");
+                        }
+                    }
+                    else
+                    {
+                        printf("Fin de la connexion\n");
+                        stop = 1;
+                    }
                 }
             }
         }
